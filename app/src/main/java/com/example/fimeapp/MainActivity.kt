40eae235af53b0b5653376a9a15566d895aa1876
saulisaias <saulisaias1501@gmail.com>
2024-07-15 05:20:37 -0600

    [ADD] login screen only
    aun falta agregar cambios en login de diseno

--- app/src/main/java/com/example/fimeapp/MainActivity.kt
+++ app/src/main/java/com/example/fimeapp/MainActivity.kt
@@ -14,18 +14,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fimeapp.databinding.ActivityLoginBinding
+import com.example.fimeapp.databinding.ActivityMainBinding
 
 import com.example.fimeapp.db_manager.DBHelper
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class MainActivity : AppCompatActivity() {
-
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window.statusBarColor = getColor(R.color.black)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -35,6 +41,12 @@ class MainActivity : AppCompatActivity() {
 
         val test = findViewById<TextView>(R.id.testTextView)
         val loginBtn = findViewById<Button>(R.id.btnLogin)
+        auth = Firebase.auth
+        binding.cerrarBtn.setOnClickListener{
+            Firebase.auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         loginBtn.setOnClickListener {
             val intent = Intent(this, Home::class.java)
