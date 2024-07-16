37ae81271c5d11f82982f7ca23cacf7c7f47c031
saulisaias <saulisaias1501@gmail.com>
2024-07-16 16:49:16 -0600

    [ADD] datos en perfil

--- app/src/main/java/com/example/fimeapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/fimeapp/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Nombre: Saul Isaias\nApellidos: Leija Soriano\nCorreo:saul.leijaso@uanl.edu.mx"
     }
     val text: LiveData<String> = _text
 }
